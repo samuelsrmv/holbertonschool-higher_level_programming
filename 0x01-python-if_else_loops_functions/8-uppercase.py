dcf10892ee67eebb3eaf418ede49860856f75bab
samuelsrmv <samuel.srmv@gmail.com>
2020-12-08 19:28:03 -0500

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in str:
-        n = ord(i)
+    for i in range(0, len(str)):
+        n = ord(str[i])
         print(chr(n), end="")
     print("")
