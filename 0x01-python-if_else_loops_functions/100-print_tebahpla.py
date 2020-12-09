b82faa6f7e2d5869c97995965e04c7a815e181c0
samuelsrmv <samuel.srmv@gmail.com>
2020-12-08 22:15:15 -0500

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(122, 98, -1):
-    if i % 2 == 0:
-        print(chr(i), end="")
-    else:
-        print(chr(i-32), end="")
+x = 0
+for i in range(122, 96, -1):
+    print(chr(i - x), end="")
+    x = 32 - x
