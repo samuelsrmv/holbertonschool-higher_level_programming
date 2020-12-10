c58f96dcbd278d3ca8656d51d9ffa253f473177e
samuelsrmv <samuel.srmv@gmail.com>
2020-12-10 15:54:06 -0500

    How to make a script dynamic!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-def add_num(long):
-    add = 0
-    for count in range(1, long):
-        add = add + int(sys.argv[count])
-    print(add)
+
+
+def print_arg(long):
+    for num_arg in range(1, long):
+        print("{:d}: {:s}".format(num_arg, sys.argv[num_arg]))
+
 
 if __name__ == "__main__":
     import sys
     long = len(sys.argv)
-    add_num(long)
+    if long == 1:
+        print("{:d} arguments.".format(long - 1))
+    elif long == 2:
+        print("{:d} argument:".format(long - 1))
+    else:
+        print("{:d} arguments:".format(long - 1))
+
+    print_arg(long)
