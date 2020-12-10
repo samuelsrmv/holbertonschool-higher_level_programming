bac873b5b8236e655c5bb52e9ce65db61ee69382
samuelsrmv <samuel.srmv@gmail.com>
2020-12-10 15:51:58 -0500

    Infinite addition

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
-
-
-def print_arg(long):
-    for num_arg in range(1, long):
-        print("{:d}: {:s}".format(num_arg, sys.argv[num_arg]))
-
+def add_num(long):
+    add = 0
+    for count in range(1, long):
+        add = add + int(sys.argv[count])
+    print(add)
 
 if __name__ == "__main__":
     import sys
     long = len(sys.argv)
-    if long == 1:
-        print("{:d} arguments.".format(long - 1))
-    elif long == 2:
-        print("{:d} argument:".format(long - 1))
-    else:
-        print("{:d} arguments:".format(long - 1))
-
-    print_arg(long)
+    add_num(long)
