8b5c099096ba2989aa9bf99b7e36e2935e30a638
samuelsrmv <samuel.srmv@gmail.com>
2021-01-19 15:32:32 -0500

    Exact same object

---
+++ 0x0A-python-inheritance/tests/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""My class
+    """
+
+
+def is_same_class(obj, a_class):
+    """
+function
+"""
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
