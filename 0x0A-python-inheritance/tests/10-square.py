4c7175b4bb83d1ef5350cf17085c6ffba6e25de3
samuelsrmv <samuel.srmv@gmail.com>
2021-01-19 18:26:38 -0500

    Square #1

---
+++ 0x0A-python-inheritance/tests/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""class Square
+    """
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ class squares
+    """
+
+    def __init__(self, size):
+        """initialize a square
+        Arguments:
+            size {int}
+        """
+
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
