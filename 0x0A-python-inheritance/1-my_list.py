8550cce2a6b80415b7079a0944139d5daccca714
samuelsrmv <samuel.srmv@gmail.com>
2021-01-19 13:42:05 -0500

    Lookup

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Write a class MyList that inherits from list
+    """
+
+
+class MyList(list):
+    """Write a class MyList that inherits from list
+    """
+    pass
+
+    def print_sorted(self):
+        """that prints the list, but sorted (ascending sort)
+        """
+        print(sorted(self))
