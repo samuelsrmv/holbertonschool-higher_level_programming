c2a2d34dc45e709d37419fa6a2c5ae8e60b0495b
samuelsrmv <samuel.srmv@gmail.com>
2021-01-20 18:22:55 -0500

    Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""module student"""
+
+
+class Student:
+    """class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize instance student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return a dictionary representation of instance student
+        """
+        is_list = True
+        if type(attrs) is not list:
+            is_list = False
+        else:
+            for i in attrs:
+                if type(i) is not str:
+                    is_list = False
+
+        if not is_list:
+            return self.__dict__
+        else:
+            dicti = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    dicti[attr] = self.__dict__.get(attr)
+            return dicti
