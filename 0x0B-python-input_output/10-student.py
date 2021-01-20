a171c5cf587e16fd958146c593cd614dbf70befa
samuelsrmv <samuel.srmv@gmail.com>
2021-01-20 18:50:02 -0500

    Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,9 +1,33 @@
 #!/usr/bin/python3
-"""class_to_json module"""
+"""module student"""
 
 
-def class_to_json(obj):
-    """dictionary description of simple data structure
-    for JSON serialization
-    """
-    return obj.__dict__
+class Student:
+    """class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize instance student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return a dictionary representation of instance student
+        """
+        is_list = True
+        if type(attrs) is not list:
+            is_list = False
+        else:
+            for i in attrs:
+                if type(i) is not str:
+                    is_list = False
+
+        if not is_list:
+            return self.__dict__
+        else:
+            dicti = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    dicti[attr] = self.__dict__.get(attr)
+            return dicti
