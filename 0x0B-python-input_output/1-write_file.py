6ed541dbfa03a1c29aec183d3de5a6fcbde2ad12
samuelsrmv <samuel.srmv@gmail.com>
2021-01-20 18:35:15 -0500

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""module
+"""module to write file
     """
 
 
-def number_of_lines(filename=""):
-    """return
+def write_file(filename="", text=""):
+    """write file with text input
     """
-    with open(filename, "rt", encoding="UTF-8") as data_file:
-        return (len(list(data_file)))
+    with open(filename, "w", encoding="utf-8") as data_file:
+        return data_file.write(text)
