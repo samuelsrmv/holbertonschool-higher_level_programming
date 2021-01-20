d313c0d5e3790bff6d672318735e5cacca1c747d
samuelsrmv <samuel.srmv@gmail.com>
2021-01-20 18:19:42 -0500

    Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""creates an Object from a “JSON file
+    """
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """creates an Object from a “JSON file
+    """
+    with open(filename, mode="r") as data_file:
+        return json.load(data_file)
