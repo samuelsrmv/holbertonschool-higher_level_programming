8634e87ede6029257ba87dcf55ec0bb73cd171c3
samuelsrmv <samuel.srmv@gmail.com>
2021-01-20 18:36:31 -0500

    Append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-"""
-module for read_lines.
-"""
+"""appends a string at the end of a text file
+    """
 
 
-def read_lines(filename="", nb_lines=0):
-    """Read n lines of a text file (UTF8) and prints it to stdout."""
-    count = len(open(filename).readlines())
-    with open(filename, 'r') as data_file:
-        if nb_lines > 0 and nb_lines < count:
-            while nb_lines:
-                print(data_file.readline(), end="")
-                nb_lines -= 1
-        else:
-            read_data = data_file.read()
-            print(read_data, end="")
+def append_write(filename="", text=""):
+    """appened string at end
+    """
+    with open(filename, mode="a") as data_file:
+        return data_file.write(text)
