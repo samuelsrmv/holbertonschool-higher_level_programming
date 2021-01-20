31df1335b809f49022d4031af588ee5293a785d8
samuelsrmv <samuel.srmv@gmail.com>
2021-01-20 18:20:24 -0500

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""script to add all arguments to a list"""
+
+import sys
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+try:
+    my_list = load_from_json_file('add_item.json')
+    my_list.extend(sys.argv[1:])
+    save_to_json_file(my_list, 'add_item.json')
+
+except FileNotFoundError:
+    save_to_json_file(sys.argv[1:], 'add_item.json')
