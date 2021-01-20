62a29c0d6323eeae0aee097c260b6e023175dae0
samuelsrmv <samuel.srmv@gmail.com>
2021-01-20 18:17:14 -0500

    Save Object to a file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module for json string
+    """
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """ json representation of object
+    """
+    return json.dumps(my_obj)
