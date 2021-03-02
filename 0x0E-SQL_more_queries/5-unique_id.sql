d34ebcdd211d7a2e716ff82a9a7b8bea061e0e5c
samuelsrmv <samuel.srmv@gmail.com>
2021-03-02 17:32:24 -0500

    Unique ID

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- script that creates the table
-CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT '1' UNIQUE (id), name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT '1' UNIQUE id, name VARCHAR(256));
