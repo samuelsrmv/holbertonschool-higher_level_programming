624a683a58f071c63fb6e490c2d7235abed041d2
samuelsrmv <samuel.srmv@gmail.com>
2021-03-02 16:43:04 -0500

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 --creates the database and user
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
