50b5c187b3aa5baf51b6d9334cda2b5501f974c1
samuelsrmv <samuel.srmv@gmail.com>
2021-03-01 22:15:38 -0500

    Full description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 -- prints the full description of the table
-SELECT * FROM INFORMATION_SCHEMA.Columns WHERE first_table = 'first_table';
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE first_table = 'first_table';
