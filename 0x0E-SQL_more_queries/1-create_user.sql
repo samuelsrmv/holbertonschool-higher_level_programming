362d1ba48b8eb395ae6ef10ed9a00e03e9b1a16f
samuelsrmv <samuel.srmv@gmail.com>
2021-03-02 16:22:12 -0500

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,6 @@
 -- creates the MySQL server user user_0d_1
-GRANT ALL ON *.* TO IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+FLUSH PRIVILEGES;
