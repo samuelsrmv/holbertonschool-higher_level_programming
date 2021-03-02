e51f10f4b71c35b2ff7b8ec078b9e09df143faa4
samuelsrmv <samuel.srmv@gmail.com>
2021-03-02 17:47:23 -0500

    States table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,3 @@
 -- script that creates the database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT SERIAL PRIMARY KEY, name VARCHAR(256),UNIQUE (id));
+CREATE TABLE IF NOT EXISTS states (id INT SERIAL PRIMARY KEY, name VARCHAR(256), UNIQUE (id));
