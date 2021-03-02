2e7b1c63f7d50b35311032a00e0992b1a7f6b595
samuelsrmv <samuel.srmv@gmail.com>
2021-03-02 15:56:31 -0500

    My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- script that lists all privileges
-SHOW GRANTS FOR 'user_0d_1'@'localhost' 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
