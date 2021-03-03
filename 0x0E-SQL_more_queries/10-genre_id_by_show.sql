050c24ee1b95281523cdcade484add8459f88b59
samuelsrmv <samuel.srmv@gmail.com>
2021-03-02 23:33:47 -0500

    Genre ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- Import the database dump
-SELECT tv_shows.title tv_show_genres.genre_id FROM hbtn_0d_tvshows ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM hbtn_0d_tvshows ORDER BY tv_shows.title and tv_show_genres.genre_id ASC;
