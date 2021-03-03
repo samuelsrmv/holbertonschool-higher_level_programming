ed5c2df16d839691781860de77b28b1501f4c52c
samuelsrmv <samuel.srmv@gmail.com>
2021-03-02 20:09:03 -0500

    Cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,3 @@
 -- creates the database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT PRIMARY KEY AUTO_INCREMENT, state_id INT FOREIGN KEY (id) REFERENCES states(id), name VARCHAR(256), UNIQUE (id));
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT PRIMARY KEY AUTO_INCREMENT, state_id INT FOREIGN KEY id REFERENCES states(id), name VARCHAR(256), UNIQUE (id));
