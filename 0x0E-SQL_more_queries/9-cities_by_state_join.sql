ad1872c76c9979131e59c02168253939f9855eef
samuelsrmv <samuel.srmv@gmail.com>
2021-03-02 22:25:11 -0500

    Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,4 @@
 --script that lists all cities contained in the database
-SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id=states.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name 
+FROM cities 
+INNER JOIN states ON cities.state_id=states.id ORDER BY cities.id ASC;
