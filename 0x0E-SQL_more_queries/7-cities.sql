9ca707b2cd803f4bd3ba93c61683ecf36872c557
samuelsrmv <samuel.srmv@gmail.com>
2021-03-02 20:34:56 -0500

    Cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,4 @@
--- hat creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
+-- hat creates the databasea and the table cities (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
@@ -6,5 +6,5 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
+    FOREIGN KEY (state.id) REFERENCES hbtn_0d_usa.states(id)
 );
