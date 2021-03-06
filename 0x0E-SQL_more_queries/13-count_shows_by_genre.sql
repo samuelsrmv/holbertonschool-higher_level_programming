bb67c6e4fe3c2643ad2146afb81be44b51bba81a
samuelsrmv <samuel.srmv@gmail.com>
2021-03-06 10:33:14 -0500

    Number of shows by genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,11 @@
+-- script that lists all genres from hbtn_0d_tvshows
+SELECT
+    name AS genre,
+    COUNT(*) AS number_of_shows
+FROM
+    tv_show_genres
+    INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY
+    tv_genres.name
+ORDER BY
+    number_of_shows DESC;
