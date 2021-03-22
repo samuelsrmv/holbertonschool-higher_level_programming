cf1fdc08351daff357ccf77f2ae379773a35834e
samuelsrmv <samuel.srmv@gmail.com>
2021-03-22 18:57:18 -0500

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ if (process.argv[2] === undefined) {
 } else if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i <= process.argv[2]; i++) {
-    console.log('x'.repeat(process.argv[2]));
+  for (let i = 1; i <= process.argv[2]; i++) {
+    console.log('X'.repeat(process.argv[2]));
   }
 }
