4aeea8a3b30d7686965fbbad843b910cea5463b9
samuelsrmv <samuel.srmv@gmail.com>
2021-03-22 14:48:34 -0500

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVar = 'C is fun';
+const myVarr = 'Python is cool';
+const myVarrr = 'JavaScript is amazing';
+console.log(myVar);
+console.log(myVarr);
+console.log(myVarrr);
