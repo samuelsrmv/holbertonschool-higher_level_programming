3e2b636246c49d0fe6a761aa2fa3c0d833c5f3d5
samuelsrmv <samuel.srmv@gmail.com>
2021-03-22 17:04:25 -0500

    Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-if (process.argv.length === 4) {
-  console.log('c is cool');
-} else if (process.argv.length === 3) {
+if (process.argv.length <= 2) {
+  console.log('undefined is undefined');
+} else if (process.argv[3] === undefined) {
   console.log('c is undefined');
 } else {
-  console.log('undefined is undefined');
+  console.log('c is cool');
 }
