81eeac39a91e1fd8036c7cd7eab642e9967df0c7
samuelsrmv <samuel.srmv@gmail.com>
2021-03-22 15:41:21 -0500

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-const args = process.argv;
-if (args.length <= 2) {
+if (process.argv.length <= 2) {
   console.log('No argument');
-} else if (args.length === 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
-  console.log('Argument found');
+  console.log('Arguments found');
 }
