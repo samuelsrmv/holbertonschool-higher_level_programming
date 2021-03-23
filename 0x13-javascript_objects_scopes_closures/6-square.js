2ac0c8d7c326c74a7cd9544ab9fdb685b7c55719
samuelsrmv <samuel.srmv@gmail.com>
2021-03-23 16:17:15 -0500

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,9 +5,9 @@ class Square extends Rectangle {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
-      }  
+    }
     for (let index = 0; index < this.height; index++) {
-        console.log(c.repeat(this.width));
+      console.log(c.repeat(this.width));
     }
   }
 }
