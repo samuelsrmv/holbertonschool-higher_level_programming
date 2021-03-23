a4dd657b9bfb82428c5631d8c4cf6c478451520a
samuelsrmv <samuel.srmv@gmail.com>
2021-03-23 16:12:22 -0500

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const Rectangle = require('./5-square');
+class Square extends Rectangle {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+      for (let index = 0; index < this.height; index++) {
+        console.log(c.repeat(this.width));
+      }
+    } else {
+      c = 'C';
+      for (let index = 0; index < this.height; index++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
