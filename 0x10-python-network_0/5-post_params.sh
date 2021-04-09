eba5c7ca38b5692d7c9ceec69e2d4ddccf8d98b3
samuelsrmv <samuel.srmv@gmail.com>
2021-04-09 10:03:11 -0500

    cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #5
-curl -X POST -d 'email=hr@holbertonschool.com' -d 'subject=I will always be here for PLD' $1
+curl -X POST -d 'subject=I will always be here for PLD' -d 'email=hr@holbertonschool.com' $1
