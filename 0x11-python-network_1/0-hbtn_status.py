e0e7e84ade86dd21caba9987474e6e4a1dc69189
samuelsrmv <samuel.srmv@gmail.com>
2021-04-09 12:44:29 -0500

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,13 @@
 import urllib.request
 
 def mystatus():
+    """what's my status
+    """
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-    print("\t- type: {}$".format(type(html)))
-    print("\t- content: {}$".format(html))
-    print("\t- utf8 content: {}$".format(html.decode('utf-8')))
+        print("\t- type: {}$".format(type(html)))
+        print("\t- content: {}$".format(html))
+        print("\t- utf8 content: {}$".format(html.decode('utf-8')))
 
 if __name__ == '__main__':
     mystatus()
