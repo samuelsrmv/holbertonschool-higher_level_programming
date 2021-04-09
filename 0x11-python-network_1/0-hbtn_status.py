70249097ed6f7183e723d9ad8fe827589301e6e6
samuelsrmv <samuel.srmv@gmail.com>
2021-04-09 12:51:04 -0500

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -13,5 +13,5 @@ def mystatus():
         print("\t- content: {}$".format(html))
         print("\t- utf8 content: {}$".format(html.decode('utf-8')))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     mystatus()
