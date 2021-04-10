25fb5476a7e98850cd1451e000f188d824c6ba43
samuelsrmv <samuel.srmv@gmail.com>
2021-04-09 22:54:47 -0500

    Response header value #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,7 +7,7 @@ import sys
 def myReque(arg):
     """what's my status"""
     x = requests.get(arg)
-    print("{}".format(x.headers['X-Request-Id']))
+    print("{}".format(x.headers.get('X-Request-Id')))
 
 if __name__ == "__main__":
     myReque(sys.argv[1])
