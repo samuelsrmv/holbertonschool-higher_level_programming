6164f342679b2eca3f5358b28a13a97d5b4d5e81
samuelsrmv <samuel.srmv@gmail.com>
2021-04-09 21:19:44 -0500

    POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""what's my status"""
+import urllib.request
+import urllib.parse
+from sys import argv
+
+def myEmail(args, email):
+    """what's my status"""
+    value = {'email': email}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(args, data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(html)
+
+if __name__ == "__main__":
+    myEmail(argv[1], argv[2])
