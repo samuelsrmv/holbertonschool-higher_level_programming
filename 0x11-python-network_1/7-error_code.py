0d2913aa504e76f650536692286c9defb2463177
samuelsrmv <samuel.srmv@gmail.com>
2021-04-09 23:30:22 -0500

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """#7"""
+from sys import argv
 import requests
-import sys
 
 
-def myErrorR(args):
-    """what's my status"""
-    try:
-        x = requests.get(args)
-        print(x.text)
-    except requests.exceptions.HTTPError as err:
-        print("Error code: {}".format(err.code))
+def Eerror():
+    """ function
+    """
+    result = requests.get(argv[1])
+    if result.status_code > 400:
+        print("Error code: {}".format(result.status_code))
+    else:
+        print(result.text)
 
 if __name__ == "__main__":
-    myErrorR(sys.argv[1])
+    Eerror()
