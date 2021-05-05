5a9d6e31a10f62fc483000eb30e19cd63fb045c4
samuelsrmv <samuel.srmv@gmail.com>
2021-05-05 14:25:05 -0500

    Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const { argv } = require('process');
+
+const arg_ = argv[2];
+const url = arg_;
+request(url, function (error, response, body) {
+  if (error) {
+    return console.log(error);
+  }
+  let count = 0;
+  const json_ = JSON.parse(body).results;
+  for (const property of json_) {
+    for (const property2 of property.characters) {
+      if (property2 === 'https://swapi-api.hbtn.io/api/people/18/') {
+        count += 1;
+      }
+    }
+  }
+  console.log(count);
+});
