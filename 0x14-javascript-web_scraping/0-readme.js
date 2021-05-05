e3503bb89bbb71954bc3d85bed20985f8dcd7717
samuelsrmv <samuel.srmv@gmail.com>
2021-05-05 12:01:53 -0500

    Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+  console.log('\n');
+});
