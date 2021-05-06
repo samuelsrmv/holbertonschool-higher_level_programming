4b95bf062be1b58baa29cce6416a3b21790c3ef0
samuelsrmv <samuel.srmv@gmail.com>
2021-05-06 09:13:48 -0500

    Who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -5,15 +5,18 @@ const { argv } = require('process');
 const arg_ = argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + arg_;
 request(url, function (error, response, body) {
-	if (error) {
-		return console.log(error);
-	}
-	const json_ = JSON.parse(body);
-		for (const property of json_.characters) {
-				request(property, function (error, response, body) {
-						const name_ = JSON.parse(body);
-						console.log(name_.name);
-		
-		}
-				)}
+  if (error) {
+    return console.log(error);
+  }
+  const json_ = JSON.parse(body);
+  for (const property of json_.characters) {
+    request(property, function (er, response, body) {
+      if (er) {
+        return console.log(er);
+      }
+      const name_ = JSON.parse(body);
+      console.log(name_.name);
+    }
+    );
+  }
 });
