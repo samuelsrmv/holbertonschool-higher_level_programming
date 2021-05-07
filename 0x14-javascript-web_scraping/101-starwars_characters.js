7bd3673a45fdfffae359d51e28bdcd74888e42cf
samuelsrmv <samuel.srmv@gmail.com>
2021-05-06 20:29:42 -0500

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -9,14 +9,24 @@ request(url, function (error, response, body) {
     return console.log(error);
   }
   const json_ = JSON.parse(body);
+  const promesas = [];
   for (const property of json_.characters) {
-    request(property, function (er, response, body) {
-      if (er) {
-        return console.log(er);
-      }
-      const name_ = JSON.parse(body).name;
-      console.log(name_);
-    }
-    );
+    const promise = new Promise(function (resolve, reject) {
+      request(property, function (er, response, body) {
+        if (er) {
+          return reject(er);
+        }
+        const name_ = JSON.parse(body).name;
+        resolve(name_);
+      });
+    });
+    promesas.push(promise);
   }
+  Promise.all(promesas).then(function (resultado) {
+    for (const x of resultado) {
+      console.log(x);
+    }
+  }).catch(function (res) {
+    console.log(res);
+  });
 });
