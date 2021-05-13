ba9435f9b6c36dd303f08744b7824111e2af0f01
samuelsrmv <samuel.srmv@gmail.com>
2021-05-12 22:22:16 -0500

    Click and turn red

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,4 +1,3 @@
-#!/usr/bin/node
-$('#red_header').click(function(){
-    $('#red_header').css({'color': '#FF0000'})
+$('#red_header').click(function () {
+  $('#red_header').css({ color: '#FF0000' });
 });
